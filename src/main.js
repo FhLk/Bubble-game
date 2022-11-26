6ed42b5142bdefa54b8fff835cae89a4fce1656a
FhLk <68800909+FhLk@users.noreply.github.com>
2022-11-26 17:15:41 +0700

    Add project

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'phaser';
+import Phaser from 'phaser';
+import GameScene from './scenes/MainMenu/GameScene';
+
+const config = {
+    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+    type: Phaser.AUTO,
+    pixelArt: true,
+    roundPixels: true,
+    parent: 'content',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+        }
+    },
+    scene: [
+        GameScene
+    ],
+
+};
+
+const game = new Phaser.Game(config);
